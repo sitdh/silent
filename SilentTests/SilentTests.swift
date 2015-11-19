489c4f4b698d73853e941a9335e7e8c8a23e41d0
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-11-19 15:35:27 +0700

    Initial Commit

---
+++ SilentTests/SilentTests.swift
@@ -0,0 +1,36 @@
+//
+//  SilentTests.swift
+//  SilentTests
+//
+//  Created by Sitdhibong Laokok on 19/11/58.
+//  Copyright © พ.ศ. 2558 Sitdhibong Laokok. All rights reserved.
+//
+
+import XCTest
+@testable import Silent
+
+class SilentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
