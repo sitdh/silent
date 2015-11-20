c0b7361a6f9e09e97739244e9e1323645e0a92ac
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-11-20 18:15:23 +0700

    Reduce test case

--- SilentTests/SilentTests.swift
+++ SilentTests/SilentTests.swift
@@ -21,17 +21,10 @@ class SilentTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testDemonstration() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(true);
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
